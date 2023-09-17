b50602711e288e420abd8a03987811b29ad6bb8f
Divyanshu Shukla <divyanshushukla82@gmail.com>
2023-09-17 19:21:17 +0530

    Update fetchData.jsx

--- src/utils/fetchData.jsx
+++ src/utils/fetchData.jsx
@@ -2,7 +2,7 @@ export const exerciseOptions = {
   method: 'GET',
   url: 'https://exercisedb.p.rapidapi.com/exercises/bodyPartList',
   headers: {
-    'X-RapidAPI-Key': '5d349be16bmshff1f55e6bc3fad4p1a55c9jsn73a8e8fd5430',
+    'X-RapidAPI-Key': 'your key',
     'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
   }
 }
@@ -14,7 +14,7 @@ export const youtubeOptions = {
     id: 'UCE_M8A5yxnLfW0KghEeajjw'
   },
   headers: {
-    'X-RapidAPI-Key': '5d349be16bmshff1f55e6bc3fad4p1a55c9jsn73a8e8fd5430',
+    'X-RapidAPI-Key': 'your key',
     'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
   }
 
